1052cf6346648868c86fdffc7e0992e328d0d9c7
dev13 <dev13@codecraftdev.com>
2023-08-14 17:09:03 -0500

    se crea el resolver de ARQ_Role

---
+++ src/resolvers/ARQ_Role.js
@@ -0,0 +1,12 @@
+const Role = require('../models/session/Role');
+const { v4: uuidv4 } = require('uuid');
+
+const ARQ_Role = async () => {}
+
+const ARQ_Role_create = async () => {}
+
+const ARQ_Role_update = async () => {}
+
+const ARQ_Role_save = async () => {}
+
+const ARQ_Role_delete = async () => {}
